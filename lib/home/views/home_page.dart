b3f624558e65d272ef1436f417a4f94cbc319aed
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-18 00:52:08 +0530

    categories added

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fundraising_fullstack/bg_gradient.dart';
 
+import 'package:fundraising_fullstack/home/widgets/category_widget.dart';
+
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   final _searchcontroller = TextEditingController();
@@ -18,130 +20,115 @@ class HomePage extends StatelessWidget {
       body: BgGradient(
         child: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30),
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20),
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
-              child: Column(children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 13,
-                    vertical: 8,
-                  ),
-                  child: TextField(
-                      controller: _searchcontroller,
-                      style: const TextStyle(color: Colors.black),
-                      decoration: InputDecoration(
-                        fillColor: Theme.of(context).colorScheme.background,
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Find fundraisers, projects, and more",
-                      )),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      color: Theme.of(context).colorScheme.surfaceTint),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 11, horizontal: 18),
-                    child: Row(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 13,
+                        vertical: 8,
+                      ),
+                      child: TextField(
+                          controller: _searchcontroller,
+                          style: const TextStyle(color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Theme.of(context).colorScheme.background,
+                            border: InputBorder.none,
+                            prefixIcon: const Icon(Icons.search),
+                            hintText: "Find fundraisers, projects, and more",
+                          )),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          color: Theme.of(context).colorScheme.surfaceTint),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 11, horizontal: 18),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Text(
+                                "Start New\nFundraising",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    overflow: TextOverflow.visible),
+                              ),
+                              Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(18),
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onTertiaryContainer),
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: Text(
+                                      "Start Now",
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 76, 68, 68),
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                  )),
+                            ]),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          const Text(
-                            "Start New\nFundraising",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(18),
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onTertiaryContainer),
-                              child: const Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  "Start Now",
-                                  style: TextStyle(
-                                      color: Color.fromARGB(255, 76, 68, 68),
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              )),
-                        ]),
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        children: [
-                          Container(
-                            height: 70,
-                            width: 70,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(15),
-                                color: Theme.of(context)
-                                    .colorScheme
-                                    .onPrimaryContainer),
-                            child: Icon(
-                              Icons.medical_information_outlined,
-                              color: Theme.of(context).colorScheme.onSecondary,
+                          InkWell(
+                            onTap: () {},
+                            child: const Category(
+                              title: 'Medical',
+                              image: 'assets/medical.png',
                             ),
                           ),
-                          SizedBox(
-                            height: 10,
+                          InkWell(
+                            onTap: () {},
+                            child: const Category(
+                              title: 'Education',
+                              image: 'assets/education.png',
+                            ),
                           ),
-                          Text('Medical')
-                        ],
+                          InkWell(
+                            onTap: () {},
+                            child: const Category(
+                              title: 'Disastor',
+                              image: 'assets/danger.png',
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: const Category(
+                              title: 'Others',
+                              image: 'assets/other.png',
+                            ),
+                          ),
+                        ]),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      'Statistics',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
                       ),
-                      Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer),
-                        child: Icon(
-                          Icons.cast_for_education_rounded,
-                          color: Theme.of(context).colorScheme.onSecondary,
-                        ),
-                      ),
-                      Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer),
-                        child: Icon(
-                          Icons.dangerous_rounded,
-                          color: Theme.of(context).colorScheme.onSecondary,
-                        ),
-                      ),
-                      Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: Theme.of(context)
-                                .colorScheme
-                                .onPrimaryContainer),
-                        child: Icon(
-                          Icons.devices_other_rounded,
-                          color: Theme.of(context).colorScheme.onSecondary,
-                        ),
-                      ),
-                    ])
-              ]),
+                    )
+                  ]),
             ),
           ),
         ),
