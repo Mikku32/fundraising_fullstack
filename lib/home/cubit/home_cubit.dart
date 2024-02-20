af5e9763c13f5c31e5f376e514c988f7e6e6ac6f
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-20 11:19:58 +0530

    home cubit

--- lib/home/cubit/home_cubit.dart
+++ lib/home/cubit/home_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:fundraising_fullstack/home/models/projects.dart';
 import 'package:meta/meta.dart';
 
@@ -6,4 +7,24 @@ part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
+
+  void getProjects() async {
+    emit(HomeLoading());
+
+    try {
+      final projects = await Dio().get(
+        "http://127.0.0.1:8000/api/project/",
+      );
+
+      final List<Projects> projectList = [];
+
+      for (var project in projects.data) {
+        projectList.add(Projects.fromJson(project));
+      }
+
+      emit(HomeSuccess(project: projectList));
+    } catch (e) {
+      emit(HomeError(e.toString()));
+    }
+  }
 }
